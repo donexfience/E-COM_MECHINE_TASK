b40f084463094723380ecbf839f7cdace27c46ed
donexfience <donex6fience@gmail.com>
2025-05-30 10:30:23 +0530

    INIT #2 (server + client ) : basic server setup done

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,46 @@
-import express from "express";
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors'
+import morgan from 'morgan'
 
-const app = express();
+class Server {
+  private app: Express;
+  private port: number;
 
-app.listen(3000, () => {
-  console.log("Server running on 3000");
-});
+  constructor() {
+    dotenv.config();
+
+    this.app = express();
+    this.port = parseInt(process.env.PORT || '3000',10);
+
+    this.configureMiddleware();
+    this.configureRoutes();
+  }
+
+  private configureMiddleware(): void {
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(
+      cors({
+        origin: ["http://localhost:5173"],
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+        credentials: true,
+      })
+    );
+    this.app.use(morgan("tiny"));
+  }
+
+  private configureRoutes(): void {
+    this.app.get('/', (req: Request, res: Response) => {
+      res.send('Hello, TypeScript Express Server!');
+    });
+  }
+
+  public start(): void {
+    this.app.listen(this.port, () => {
+      console.log(`Server running at http://localhost:${this.port} on ${new Date().toISOString()}`);
+    });
+  }
+}
+
+export default Server;
